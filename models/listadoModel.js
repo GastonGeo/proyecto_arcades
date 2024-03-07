a3caaf6ee9a27422cb2b2a16005a1f8021efcc92
Gaston Oubina <geogastono@gmail.com>
2024-03-07 20:00:01 +0100

    modified CRUD listado novedades

--- models/listadoModel.js
+++ models/listadoModel.js
@@ -6,10 +6,42 @@ async function getListado() {
     return rows;
 }
 
+
+
 async function deleteNovedadById(id) {
     const query = "delete from listado where id = ? ";
     const rows = await pool.query(query, [id]);
     return rows;
 }
 
-module.exports = { getListado, deleteNovedadById }
+async function insertNovedad(obj) {
+    try {
+        const query = "insert into listado set ?";
+        const rows = await pool.query(query, [obj])
+        return rows; 
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function getNovedadById(id) {
+    const query = "select * from listado where id=? ";
+    const rows = await pool.query(query,[id]);
+    return rows[0];
+}
+
+async function modificarNovedadById(obj, id) {
+    try {
+        const query = "update listado set ? where id=? ";
+        const rows = await pool.query(query,[obj, id]);
+    return rows;
+    } catch (error) {
+        throw error;
+    }
+    
+}
+
+
+module.exports = { getListado, deleteNovedadById, insertNovedad, getNovedadById, modificarNovedadById }
--- routes/admin/listado.js
+++ routes/admin/listado.js
@@ -7,7 +7,7 @@ const listadoModel = require('./../../models/listadoModel');
 router.get('/', async function (req, res, next) {
     const listado = await listadoModel.getListado();
     res.render('admin/listado', {
-        layout: '/admin/layout',
+        layout: 'admin/layout',
         usuario: req.session.nombre,
         listado
     });
@@ -19,5 +19,66 @@ router.get('/eliminar/:id', async (req, res, next) => {
     await listadoModel.deleteNovedadById(id);
     res.redirect('/admin/listado')
 });
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })
+});
+
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+            await listadoModel.insertNovedad(req.body);
+            res.redirect('/admin/listado')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error: true,
+                message: 'Todos los campos son requeridos'
+            })
+
+        }
+    } catch (error) {
+        console.log(error)
+        res.render('admin/agregar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargó la novedad'
+        })
+    }
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+    const id = req.params.id; 
+    const novedad = await listadoModel.getNovedadById(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    });
+});
+
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        const obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+
+        console.log(obj)
+            await listadoModel.modificarNovedadById(obj, req.body.id);
+            res.redirect('/admin/listado')
+        } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modificó la novedad'
+        })
+    }
+})
+
 
 module.exports = router;
